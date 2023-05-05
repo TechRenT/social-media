69000f5e3869df5708a7a43501a0957db9a77666
Renelle Tigue <titingrenelle@gmail.com>
2023-05-05 10:33:51 +0800

    Started adding javascript to this project

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let root = document.getElementsByClassName('root')[0];
+
+root.style.cssText = `
+background: url('https://wallpaperaccess.com/full/636909.jpg');
+background-size: cover;
+background-position: center;
+`;
+
+console.log(root);
